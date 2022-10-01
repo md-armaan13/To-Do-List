d6801f170e81da4a84c994967fa98d6e613739c7
MD ARMAAN ANSARI <arman.kanpur4080@gmail.com>
2022-10-01 17:46:10 +0530

    fixing first bug

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
  const router = express.Router();
- const homeController = require('../controllers/home_control');
+ const homeController = require('../controllers/home-control');
 
 
  console.log("Router initialised");
 
  router.get('/',homeController.home);// calling home controller when '/' is called
+
+
+ module.exports=router;
