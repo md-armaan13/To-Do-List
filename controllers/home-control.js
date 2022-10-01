27508b2c73f27191516224351d7904f7eeac6273
MD ARMAAN ANSARI <arman.kanpur4080@gmail.com>
2022-10-01 17:35:24 +0530

    adding views and controllers

---
+++ controllers/home-control.js
@@ -0,0 +1,6 @@
+module.exports.home= (req,res)=>{ // exporting the fuction so that router can use
+
+    return res.render('home',{
+        title : "To-Do-List",
+    });
+};
--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const port=8000;
 app.use('/',require('./routes/index')); 
 
 
+// SET UP VIEW ENGINE
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+
+
 app.listen(port,function(err) {
 
     if(err) { 
